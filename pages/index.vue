e84b191a455d851562c74c1d04f56fd3a139499c
Matthew Pence <matt@ohiosveryown.co>
2021-01-05 20:48:19 +0000

    Started header

--- pages/index.vue
+++ pages/index.vue
@@ -1,137 +1,66 @@
 <!-- layout -->
 <template>
-  <main>
-    <div class="font-alpina">Alpina</div>
-    <div class="font-americana">Americana</div>
-    <div class="font-respira">Repsira</div>
-    <div class="font-saol">Saol</div>
-    <div class="font-steinbeck">Steinbeck</div>
+  <main class="scrollContainer" data-scroll-container>
+    <header data-scroll-section>
+      <div class="content">
+        <h1 data-scroll data-scroll-speed="-1" class="our font-alpina font-size-xl uc">Our</h1>
+        <h1 data-scroll data-scroll-speed="-2" class="favorite font-saol font-size-xl uc">Favorite</h1>
+        <h1 data-scroll data-scroll-speed="-1.5" class="albums font-alpina font-size-xl uc">
+          Albums
+          <span class="established font-saol font-size-lg">Est. 2018</span>
+        </h1>
+      </div>
+    </header>
 
-    <ul>
-      <li v-for="post of posts" :key="post.slug">
-        <NuxtLink :to="post.slug">
-          <h1 class="font-alpina uc">{{ post.title }}</h1>
-          <h2>{{ post.description }}</h2>
-        </NuxtLink>
-      </li>
-    </ul>
-    <div class="scrollContainer" data-scroll-container>
-      <main>
-        <section data-scroll-section>
-          <h1 data-scroll>Hey</h1>
-          <p data-scroll>👋</p>
-        </section>
-        <section data-scroll-section>
-            <h2 data-scroll data-scroll-speed="-1">What's up?</h2>
-            <p data-scroll data-scroll-speed="-2">😬</p>
-        </section>
-      </main>
-    </div>
+
+
+
+
+
+    <section data-scroll-section>
+      <h1 data-scroll>Hey</h1>
+      <p data-scroll>👋</p>
+    </section>
+    <section data-scroll-section>
+        <h2 data-scroll data-scroll-speed="-1">What's up?</h2>
+        <p data-scroll data-scroll-speed="-2">😬</p>
+    </section>
   </main>
 </template>
 
 <!-- style -->
 <style lang="scss" scoped>
   @import '~/static/style/grid.scss';
-  html.has-scroll-dragging {
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none; }
 
-  .has-scroll-smooth body {
-    overflow: hidden; }
+  .content {
+    display: flex;
+    flex-direction: column;
+    background: url('/img/classical-01.jpg') no-repeat 72% center;
+    background-size: auto 64%;
 
-  .has-scroll-smooth [data-scroll-container] {
-    min-height: 100vh; }
+    @include breakpoint(md) {
+      justify-content: center;
+      height: 100%;
+    }
+  }
 
-  [data-scroll-direction="horizontal"] [data-scroll-container] {
-    white-space: nowrap;
-    max-height: 100vh;
-    display: inline-block;
-    white-space: nowrap; }
+  .our {
+    @include breakpoint(md) { padding-left: 11.2rem; }
+  }
 
-  [data-scroll-direction="horizontal"] [data-scroll-section] {
-    display: inline-block;
-    vertical-align: top;
-    white-space: nowrap;
-    height: 100%; }
+  .favorite {
+    @include breakpoint(md) { padding: 2rem 0 0 34.4rem; }
+  }
 
-  .c-scrollbar {
-    position: absolute;
-    right: 0;
-    top: 0;
-    width: 11px;
-    height: 100%;
-    transform-origin: center right;
-    transition: transform 0.3s, opacity 0.3s;
-    opacity: 0; }
-    .c-scrollbar:hover {
-      transform: scaleX(1.45); }
-    .c-scrollbar:hover, .has-scroll-scrolling .c-scrollbar, .has-scroll-dragging .c-scrollbar {
-      opacity: 1; }
-    [data-scroll-direction="horizontal"] .c-scrollbar {
-      width: 100%;
-      height: 10px;
-      top: auto;
-      bottom: 0;
-      transform: scaleY(1); }
-      [data-scroll-direction="horizontal"] .c-scrollbar:hover {
-        transform: scaleY(1.3); }
+  .albums {
+    @include breakpoint(md) { margin-top: -1.2rem; }
+  }
 
-  .c-scrollbar_thumb {
-    position: absolute;
-    top: 0;
-    right: 0;
-    background-color: black;
-    opacity: 0.5;
-    width: 7px;
-    border-radius: 10px;
-    margin: 2px;
-    cursor: -webkit-grab;
-    cursor: grab; }
-    .has-scroll-dragging .c-scrollbar_thumb {
-      cursor: -webkit-grabbing;
-      cursor: grabbing; }
-    [data-scroll-direction="horizontal"] .c-scrollbar_thumb {
-      right: auto;
-      bottom: 0; }
+  .established {
+    text-transform: none;
+    @include breakpoint(md) { margin-left: 34.8rem; }
+  }
 
-      /* ***** */
-      body {
-        margin: 0;
-        max-height: 100vh;
-        overflow: hidden;
-      }
-
-      .scrollContainer {
-        margin: 0;
-        // border: 4px solid green;
-        max-height: 100vh;
-        /* width: 80vw;
-        overflow: hidden; */
-      }
-
-      main {
-        // border: 4px solid yellow;
-        width: 100%;
-        max-height: 100vh;
-        /* display: flex; */
-        /* border: 4px solid red; */
-        /* width: 70vw; height: 100vh;
-        overflow: scroll; */
-      }
-
-      section {
-        /* display: grid;
-        place-items: center; */
-        margin: 20px;
-        width: 100vw; min-height: 80vh;
-        background: pink;
-        /* flex-shrink: 0; */
-      }
-
-      .in-view { background: red; }
 </style>
 
 <!-- logic -->
